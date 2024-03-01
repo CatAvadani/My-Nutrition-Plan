71c5e4c7e2d8873d5b7f0f0f00e66152570d666d
CatAvadani <catalinaavadani@gmail.com>
2024-03-01 01:01:24 +0100

    Update nav transparency.

--- src/components/styles/Navigation.styled.tsx
+++ src/components/styles/Navigation.styled.tsx
@@ -10,7 +10,7 @@ export const Navigation = styled.div`
   max-width: 90%;
   margin: 0 auto;
   padding: 0 2rem;
-  background-color: rgba(256, 256, 256, 0.6);
+  background-color: rgba(256, 256, 256, 0.8);
   border-radius: 5rem;
   color: black;
   box-shadow: 0 2px 2px rgba(0, 0, 0, 0.1);
